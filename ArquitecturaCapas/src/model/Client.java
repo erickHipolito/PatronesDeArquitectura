a3fe850b82628fc0855bba8085fa241d03ef232f
erickHipolito <erickhero92@gmail.com>
2019-02-13 16:34:55 -0600

    Primer Commit Cada colaborador tendra que decir que modifico y cuando

---
+++ ArquitecturaCapas/src/model/Client.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the client database table.
+ * 
+ */
+@Entity
+@Table(name="client")
+@NamedQuery(name="Client.findAll", query="SELECT c FROM Client c")
+public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private ClientPK id;
+
+	@Column(name="billing_address")
+	private int billingAddress;
+
+	@Column(name="contact_email")
+	private String contactEmail;
+
+	@Column(name="contact_password")
+	private String contactPassword;
+
+	public Client() {
+	}
+
+	public ClientPK getId() {
+		return this.id;
+	}
+
+	public void setId(ClientPK id) {
+		this.id = id;
+	}
+
+	public int getBillingAddress() {
+		return this.billingAddress;
+	}
+
+	public void setBillingAddress(int billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+
+	public String getContactEmail() {
+		return this.contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public String getContactPassword() {
+		return this.contactPassword;
+	}
+
+	public void setContactPassword(String contactPassword) {
+		this.contactPassword = contactPassword;
+	}
+
+}
---
+++ ArquitecturaCapas/src/model/ClientPK.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * The primary key class for the client database table.
+ * 
+ */
+@Embeddable
+public class ClientPK implements Serializable {
+	//default serial version id, required for serializable classes.
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="client_name")
+	private String clientName;
+
+	@Column(name="client_department_number")
+	private short clientDepartmentNumber;
+
+	public ClientPK() {
+	}
+	public String getClientName() {
+		return this.clientName;
+	}
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+	public short getClientDepartmentNumber() {
+		return this.clientDepartmentNumber;
+	}
+	public void setClientDepartmentNumber(short clientDepartmentNumber) {
+		this.clientDepartmentNumber = clientDepartmentNumber;
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof ClientPK)) {
+			return false;
+		}
+		ClientPK castOther = (ClientPK)other;
+		return 
+			this.clientName.equals(castOther.clientName)
+			&& (this.clientDepartmentNumber == castOther.clientDepartmentNumber);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.clientName.hashCode();
+		hash = hash * prime + ((int) this.clientDepartmentNumber);
+		
+		return hash;
+	}
+}
